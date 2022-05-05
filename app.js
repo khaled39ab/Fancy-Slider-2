8f964f02b1e929e8bf13a95a6f53799f2b95fee9
khaled39ab <khaled39ab@gmail.com>
2022-05-05 20:09:25 +0600

    slider's do not take negative time

--- app.js
+++ app.js
@@ -68,6 +68,10 @@ const createSlider = () => {
   // hide image aria
   imagesArea.style.display = 'none';
   const duration = document.getElementById('duration').value || 1000;
+  if (duration < 5){
+    alert("Negative timing not allowed")
+    return;
+  }
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
